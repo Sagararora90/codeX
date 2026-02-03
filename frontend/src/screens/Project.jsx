470bdcd6981096fe375c9f869a9767678eb57e54
foryou927 <unmef5@gmail.com>
2026-02-03 07:12:26 +0530

    Fix deployment: correct case-sensitive import of webContainer

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -11,7 +11,7 @@ import {
 } from "../config/socket";
 import Markdown from "markdown-to-jsx";
 import hljs from "highlight.js";
-import { getWebContainer } from "../config/webcontainer";
+import { getWebContainer } from "../config/webContainer";
 
 function SyntaxHighlightedCode(props) {
   const ref = useRef(null);
